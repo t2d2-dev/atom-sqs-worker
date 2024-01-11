cac81077f860b303390ab574ddb6c67b61a67891
Badri Hiriyur <badri@t2d2.ai>
2024-01-11 13:34:40 -0500

    SQS worker updates

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import os
 import platform
 import subprocess
 import traceback
+from datetime import datetime
 from signal import SIGINT, SIGTERM, signal
 
 import boto3
@@ -14,16 +15,28 @@ import psutil
 import requests
 import watchtower
 from docker.errors import ContainerError
-from docker.types import LogConfig, Mount
+from docker.types import Mount
+from pymongo import MongoClient
 
-MEM_LIMIT = os.getenv("MEM_LIMIT", "8g")
-MEMSWAP_LIMIT = os.getenv("MEMSWAP_LIMIT", "8g")
+# AWS parameters
 AWS_ACCOUNT = os.getenv("AWS_ACCOUNT", "910371487650")
 AWS_REGION = os.getenv("AWS_REGION", "us-east-1")
 QUEUE_NAME = os.getenv("SQS_QUEUE", "atomQueueStandard")
-WAIT_TIME = int(os.getenv("WAIT_TIME", "10"))
-VISIBILITY_TIMEOUT = int(os.getenv("VISIBILITY_TIMEOUT", "3600"))
+SECRETS_NAME = os.getenv("SECRETS_NAME", "production-atom-celery-queue-1-zPDi3C")
 LOGS_GROUP = os.getenv("LOGS_GROUP", "atom_workers")
+
+# Database connection
+MONGO_URL = os.getenv("MONGO_URL", "")
+MONGO_DATABASE = os.getenv("MONGO_DATABASE", "atom")
+MONGO_COLLECTION = os.getenv("MONGO_COLLECTION", "tasks")
+MEM_LIMIT = os.getenv("MEM_LIMIT", "8g")
+MEMSWAP_LIMIT = os.getenv("MEMSWAP_LIMIT", "8g")
+
+# Queue listener params
+WAIT_TIME = int(os.getenv("WAIT_TIME", "10"))
+VISIBILITY_TIMEOUT = int(os.getenv("VISIBILITY_TIMEOUT", "7200"))
+
+# Paths and mounts
 ROOT_FOLDER = "/tmp"
 STANDARD_MOUNT_PATH = "/data"
 MODELS_MOUNT_PATH = "/models"
@@ -43,6 +56,20 @@ class SignalHandler:
         self.received_signal = True
 
 
+def get_secrets():
+    """Get secrets for environment"""
+    if not SECRETS_NAME:
+        return {}
+    secrets_arn = (
+        f"arn:aws:secretsmanager:{AWS_REGION}:{AWS_ACCOUNT}:secret:{SECRETS_NAME}"
+    )
+    client = boto3.client("secretsmanager")
+    response = client.get_secret_value(
+        SecretId=secrets_arn,
+    )
+    return json.loads(response["SecretString"])
+
+
 def get_system_info():
     """Retrieve system information"""
     try:
@@ -56,8 +83,8 @@ def get_system_info():
         info["cpu_percent"] = psutil.cpu_percent()
         info["mem"] = str(round(psutil.virtual_memory().total / (1024.0**3))) + " GB"
         info["mem_percent"] = psutil.virtual_memory().percent
-        info["disk"] = str(round(psutil.disk_usage('/').total / (1024.0**3))) + " GB"
-        info["disk_percent"] = psutil.disk_usage('/').percent
+        info["disk"] = str(round(psutil.disk_usage("/").total / (1024.0**3))) + " GB"
+        info["disk_percent"] = psutil.disk_usage("/").percent
 
         return info
     except Exception as e:
@@ -95,14 +122,14 @@ def get_instance_data():
     return info
 
 
-def set_logger(task_id, level='info'):
+def set_logger(task_id, level="info"):
     """Setup logging to cloudwatch"""
     logging_levels = {
-        'debug': logging.DEBUG,
-        'info': logging.INFO,
-        'warning': logging.WARNING,
-        'error': logging.ERROR,
-        'critical': logging.CRITICAL
+        "debug": logging.DEBUG,
+        "info": logging.INFO,
+        "warning": logging.WARNING,
+        "error": logging.ERROR,
+        "critical": logging.CRITICAL,
     }
     logging.basicConfig(level=logging_levels[level])
     handler = watchtower.CloudWatchLogHandler(
@@ -116,10 +143,75 @@ def set_logger(task_id, level='info'):
     logging.getLogger(task_id).addHandler(handler)
 
 
-def status_update(task_id, message):
+def status_update(task_id, status, sysinfo=None):
     """Update status in DB"""
-    logger = logging.getLogger(task_id)
-    logger.info("TODO: Updating status %s:%s", task_id, message)
+    try:
+        logger = logging.getLogger(task_id)
+        logger.info("Updating status %s:%s", task_id, status)
+        mongo = MongoClient(MONGO_URL)
+        db = mongo[MONGO_DATABASE]
+        collection = db[MONGO_COLLECTION]
+        now = datetime.now()
+
+        # Insert if not found
+        task = collection.find_one({"task_id": task_id})
+
+        update = {"status": status, "updated_at": now}
+        if sysinfo:
+            update["agent"] = sysinfo
+
+        if status == "running":
+            delta = now - task.get("created_at", now)
+            update["duration_queue"] = delta.days * 86400 + delta.seconds
+
+        elif status == "completed":
+            delta = now - task.get("updated_at", now)
+            update["duration"] = delta.days * 86400 + delta.seconds
+
+        result = collection.update_one({"task_id": task_id}, {"$set": update})
+        return result
+
+    except Exception as err:
+        print("*WARNING* Could not update task ", err)
+
+
+def check_message_status(task_id):
+    """Check to see if message is stopped / cancelled"""
+    try:
+        logger = logging.getLogger(task_id)
+        logger.info("Checking task status %s", task_id)
+        mongo = MongoClient(MONGO_URL)
+        db = mongo[MONGO_DATABASE]
+        collection = db[MONGO_COLLECTION]
+        now = datetime.now()
+
+        # Insert if not found
+        task = collection.find_one({"task_id": task_id})
+        if not task:
+            collection.insert_one(
+                {
+                    "project_id": 0,
+                    "created_by": "unknown",
+                    "task_name": "unknown",
+                    "task_id": task_id,
+                    "status": "running",
+                    "created_at": now,
+                    "updated_at": now,
+                    "duration_queue": 0,
+                    "duration": 0,
+                    "agent": {},
+                }
+            )
+            return False
+
+        task_status = task.get("status", "running")
+        if task_status in ("cancelled", "stopped", "killed"):
+            return True
+
+        return False
+    except Exception as err:
+        print("*WARNING* Could not check task status ", err)
+        return False
 
 
 def run_container(dkr, task_id):
@@ -136,9 +228,7 @@ def run_container(dkr, task_id):
                 source=f"{MODELS_FOLDER}", target=f"{MODELS_MOUNT_PATH}", type="bind"
             ),
         ]
-        env = dict(os.environ)
-        lc = LogConfig(type=LogConfig.types.JSON, config={"max-file": "100"})
-
+        env = get_secrets()
         device_requests = []
         try:
             _ = subprocess.check_output("nvidia-smi")
@@ -167,7 +257,6 @@ def run_container(dkr, task_id):
             mounts=mounts,
             environment=env,
             network_mode="host",
-            log_config=lc,
             mem_limit=MEM_LIMIT,
             memswap_limit=MEMSWAP_LIMIT,
             device_requests=device_requests,
@@ -200,14 +289,23 @@ def process_message(msg):
     config = event.get("config", {})
 
     # Setup cloudwatch logger
-    set_logger(task_id, event.get('log_level', "info"))
+    set_logger(task_id, event.get("log_level", "info"))
     logger = logging.getLogger(task_id)
 
     # Log system info
-    logger.info(get_instance_data())
+    sysinfo = get_instance_data()
+    logger.info(sysinfo)
+
+    # Check to see if message is cancelled or stopped
+    if check_message_status(task_id):
+        return {
+            "success": False,
+            "function": "process_message",
+            "err": "Task is cancelled",
+        }
 
     # Update status
-    status_update(task_id, "running")
+    status_update(task_id, status="running", sysinfo=sysinfo)
 
     # Get input
     logger.debug("Parsed Message: \nID: %s \nCONFIG: %s", task_id, config)
@@ -241,7 +339,7 @@ def process_message(msg):
 
 
 #############################################################################
-# MAIN ENTRY POINT
+# MAIN QUEUE LISTENER
 #############################################################################
 def main():
     """Main fn: SQS Listener and message processor"""
